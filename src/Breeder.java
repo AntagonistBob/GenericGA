b9001662a07ee9831afaaeffbcc2e8d2f764b7f4
Paul Cloete <Paul Cloete>
2016-03-19 17:20:22 +0200

    Implemented breeding function
    -Implemented structure of Breeder. Other than fixing a couple of TODO's the basic structure is almost complete
    -Changed Candidate interface's breedWith method to return a Queue, which works well in Breeder's breed function

--- src/Breeder.java
+++ src/Breeder.java
@@ -1,34 +1,58 @@
-import java.util.ArrayList;
-
 /**
  * Created by Paul Cloete on 3/19/2016.
  */
+
+import java.util.*;
+
 public class Breeder  {
 
     private int populationSize;
     private int numberOfEliteCandidates;
-    private ArrayList<Candidate>candidates;
+    private Vector<Candidate> candidates;
+    private final Random random = new Random(System.currentTimeMillis());
 
-    public Breeder() {
-        //Initialize with population + size + number of elite units
-        //Initialize population
-        //Evaluate current population (sort)
-        //Breed loop
-            //Select parents
-            //Breed parents
-            //Mutate if necessary
-            //Evaluate child/ren
-            //Replace random/weakest unit in population with child
-            //Age?
-        //Repeat
-    }
-
-    public Breeder(ArrayList<Candidate> candidates, int populationSize, int numberOfEliteCandidates) {
+    public Breeder(Vector<Candidate> candidates, int populationSize, int numberOfEliteCandidates) {
         this.candidates = candidates;
         this.populationSize = populationSize;
+        if(this.candidates.size() != populationSize){
+            //TODO throw some sort of inconsistency exception
+        }
+
         this.numberOfEliteCandidates = numberOfEliteCandidates;
+
+        sortCandidates(this.candidates);
+        breed();
     }
 
+    private void breed() {
+        //TODO threading, using thread safe data structures. requires some more thought
+        //TODO optimization, requires some thought
+        do {
+            //TODO mutation
+            int firstParentIndex = this.random.nextInt(this.populationSize);
+            int secondParentIndex = this.random.nextInt(this.populationSize);
+            Candidate firstParent = this.candidates.get(firstParentIndex);
+            Candidate secondParent = this.candidates.get(secondParentIndex);
 
+            Queue<Candidate> children = firstParent.breedWith(secondParent);
+            int numberOfChildren = children.size();
+
+            int indexOfFirstCandidateToRemove = this.populationSize - numberOfChildren;
+            for (int i = this.numberOfEliteCandidates; i < this.populationSize; i++) { //Elite candidates don't age
+                this.candidates.get(i).age();
+                if(i == indexOfFirstCandidateToRemove){
+                    this.candidates.setElementAt(children.poll(), i);
+                }
+            }
+
+            sortCandidates(this.candidates);
+        }while(true); //TODO end condition
+    }
+
+    private void sortCandidates(Vector<Candidate> candidates) {
+        Comparator<Candidate> comparator = candidates.get(0).fitnessComparator();  //This assumes that the comparator each candidate returns is the same.
+        candidates.sort(comparator);                                               //Alternative is to pass a comparator in when you create the breeder,
+                                                                                    //But i prefer this method more
+    }
 
 }
--- src/Candidate.java
+++ src/Candidate.java
@@ -1,8 +1,13 @@
 /**
  * Created by Paul Cloete on 3/19/2016.
  */
+
+import java.util.Comparator;
+import java.util.Queue;
+
 interface Candidate {
-    public Candidate[] breedWith(Candidate[] parents);
-    public String fitness();
+    public Queue<Candidate> breedWith(Candidate mate); //Assumes 2 parent breeding atm. Can be expanded at a later stage
     public void mutate();
+    public void age();
+    public Comparator<Candidate> fitnessComparator();
 }
